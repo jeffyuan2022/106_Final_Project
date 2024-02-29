633c9c20fd8e89c05a3f07d9235e7636abc6a12c
Yiheng Yuan <yiy159@ucsd.edu>
2024-02-28 16:53:34 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,69 @@
 <script>
-  // Write your JS here, or import other files
+  import Scroller from "@sveltejs/svelte-scroller";
+
+  let count, index, offset, progress;
 </script>
+<Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+  <div class="background" slot="background">
+    <Map {index} />
+    <div class="progress-bars">
+      <p>current section: <strong>{index + 1}/{count}</strong></p>
+      <progress value={count ? (index + 1) / count : 0} />
 
-<main>
-  <h1>Svelte template</h1>
+      <p>offset in current section</p>
+      <progress value={offset || 0} />
 
-  <p>Write your HTML here</p>
-</main>
+      <p>total progress</p>
+      <progress value={progress || 0} />
+    </div>
+  </div>
 
+  <div class="foreground" slot="foreground">
+    <section>This is the first section.</section>
+    <section>This is the second section.</section>
+    <section>This is the third section.</section>
+  </div>
+
+</Scroller>
 <style>
-  /* Write your CSS here */
+  /* .background {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    outline: green solid 3px;
+  } */
+
+  /* .foreground {
+    width: 50%;
+    margin: 0 auto;
+    height: auto;
+    position: relative;
+    outline: red solid 3px;
+  } */
+
+  .progress-bars {
+    position: absolute;
+    background: rgba(170, 51, 120, 0.2) /*  40% opaque */;
+    visibility: hidden;
+  }
+
+  section {
+    height: 80vh;
+    background-color: rgba(255, 255, 255, 0.2); /* 20% opaque */
+    /* color: white; */
+    outline: rgb(255, 255, 255) solid 3px;
+    text-align: center;
+    max-width: 750px; /* adjust at will */
+    color: black;
+    padding: 1em;
+    margin: 0 0 2em 0;
+  }
 </style>
